805ba914ee50d3bc549f321c359282b515e9bd1d
wow5468 <wow5468@naver.com>
2017-07-07 14:18:50 +0900

    ".gitignore" , 설정 수정

--- forman_server/settings.py
+++ forman_server/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'forman_server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
